983fad470bf78da6ae22e8622ac8341f5729205d
andreaskundig <andreas.kuendig@gmx.ch>
2021-04-17 22:12:17 +0200

    add downlad upload and clear buttons

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function canvasHtml(config) {
              width="${w}px"
              height="${h}px"
              style="margin:${y}px ${x}px;
-                    border: 1px solid;
+                    background-color: white;
                     position: absolute;" ></canvas> `;
 }
 
@@ -32,19 +32,27 @@ function createSheet(parent, canvasCount, d, state){
     const canvasConfigs = [...Array(canvasCount)].map((_, i) => ({
         w: d.size.w,
         h: d.size.h,
-        x: d.margin.left + i * (d.margin.left + d.size.w),
+        x: d.margin.left +
+            i * (d.margin.left + d.size.w ),
         y: d.margin.top
     }));
-    const totalWidth = canvasConfigs.length *(d.size.w + d.margin.left);
+    const totalWidth = d.margin.left +
+          canvasConfigs.length *(d.size.w + d.margin.left);
+    const totalHeight = d.size.h + d.margin.top * 2;
     parent.insertAdjacentHTML('afterbegin',
-      `<div class="sheet" style="position: relative">
+      `<div class="sheet"
+            style="position: relative;
+                   background-color: grey;
+                   width: ${totalWidth}px;
+                   height: ${totalHeight}px; ">
         ${canvasConfigs.map(canvasHtml).join('')}
         <div class="receiver"
              style="position: absolute;
                     width: ${totalWidth}px;
-                    height: ${d.size.h + d.margin.top + 20}px;
+                    height: ${d.size.h + d.margin.top * 2}px;
                     z-index:1;"></div>
-       </div>`);
+       </div>
+`);
     const receiver = parent.querySelector('.receiver');
     const canvasEls = parent.querySelectorAll('canvas');
     const canvases = canvasConfigs.map((canvConf, i) => {
@@ -52,19 +60,55 @@ function createSheet(parent, canvasCount, d, state){
         const context = canvas.getContext('2d');
         return Object.assign(canvConf,{i, canvas, context})
     })
+    // addPersistenceButtons(parent, canvases[0].context);
     return {receiver, canvases, state};
+}
+
+function addButtons(parent, canvases, state) {
+    parent.insertAdjacentHTML('beforeend',
+        `<a class="download" href="#" target="_blank">download</a>
+         <a class="upload" href="#" >upload</a>
+         <input type="file" class="imageLoader"
+                name="imageLoader" style="display:none"/>
+        <a class="clear" href="#" >clear</a>
+`);
+    const srcCanvas = canvases[0];
+    const context = srcCanvas.context;
+    const download = parent.querySelector('.download');
+    download.addEventListener('click', function() {
+        this.href = context.canvas.toDataURL('image/png');
+    }, false);
+    const clearAll = () =>
+        canvases.forEach(c => {
+            const ctx = c.context;
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        });
+    const clear = parent.querySelector('.clear');
+    clear.addEventListener('click', clearAll, false);
+    const imageLoader = parent.querySelector('.imageLoader')
+    imageLoader.addEventListener('change', (changeEvent) => {
+        var reader = new FileReader();
+        reader.addEventListener('load', (event) => {
+            var img = new Image();
+            img.addEventListener('load', () => {
+                clearAll();
+                srcCanvas.context.drawImage(img, 0, 0);
+                copySrcCanvas(srcCanvas, canvases, state.page);
+            });
+            img.src = event.target.result;
+        });
+        reader.readAsDataURL(changeEvent.target.files[0]);
+    }, false);
+    const upload = parent.querySelector('.upload')
+    upload.addEventListener('click', function() {
+        imageLoader.click();
+    }, false);
 }
 
 const byId = (id) => document.getElementById(id);
 const canvasParentId = 'canvas-parent';
 
-function correctCoordinates([x, y], dims) {
-    const newX = x - dims.margin.left;
-    const newY = y - dims.margin.top;
-    return [newX, newY];
-}
-
-function addDrawingListeners(sheet, dims){
+function addDrawingListeners(sheet){
     const {receiver, canvases, state} = sheet;
     state.drawing = { isDrawing: false, x: 0, y: 0 };
     receiver.addEventListener('mousedown', e => {
@@ -76,7 +120,7 @@ function addDrawingListeners(sheet, dims){
         const x = e.offsetX;
         const y = e.offsetY;
         if (state.drawing.isDrawing === true) {
-            drawLines(canvases, x, y, state, dims);
+            drawLines(canvases, x, y, state);
             state.drawing.x = x;
             state.drawing.y = y;
         }
@@ -85,33 +129,43 @@ function addDrawingListeners(sheet, dims){
         const x = e.offsetX;
         const y = e.offsetY;
         if (state.drawing.isDrawing === true) {
-            drawLines(canvases, x, y, state, dims);
+            drawLines(canvases, x, y, state);
             state.drawing.x = x;
             state.drawing.y = y;
             state.drawing.isDrawing = false;
         }
     }
     receiver.addEventListener('mouseup', endDrawing);
-    // eventReceiver.addEventListener('mouseout', endDrawing);
 }
 
-function drawLines(canvases, x, y, state, dims) {
-    const currentCanvas = canvases.find(c => canvasContains(c, x, y));
+function drawLines(canvases, x, y, state) {
+    const srcCanvas = canvases.find(c => canvasContains(c, x, y));
+    if(!srcCanvas){ return; }
     const {page, lineStyle} = state;
-    const [srcCanvas, ...destCanvases] = canvases;
-    const [srcOrder, ...destOrders] = page;
     const oldXY = [state.drawing.x, state.drawing.y];
-    const from = correctCoordinates(oldXY, dims);
-    const to = correctCoordinates([x, y], dims);
+    const from = canvasCoordinates(oldXY, srcCanvas);
+    const to = canvasCoordinates([x, y], srcCanvas);
     drawLine(srcCanvas.context, from, to, lineStyle);
-    const w = srcCanvas.w/2;
-    const h = srcCanvas.h/2;
-    destCanvases.forEach((destCanvas,i) => {
-        const destOrder = destOrders[i];
+    copySrcCanvas(srcCanvas, canvases, page);
+}
+
+function copySrcCanvas(srcCanvas, canvases, page){
+    const srcOrder = page[srcCanvas.i];
+    const destCanvases = canvases.filter(c => c.i != srcCanvas.i);
+    destCanvases.forEach(destCanvas => {
+        const destOrder = page[destCanvas.i];
         const flip = srcOrder.up != destOrder.up;
+        const w = srcCanvas.w / 2;
+        const h = srcCanvas.h / 2;
         copyCanvas(srcCanvas, srcOrder, destCanvas, destOrder,
                    w, h, flip);
     });
+}
+
+function canvasCoordinates([x, y], canvasConfig) {
+    const newX = x - canvasConfig.x;
+    const newY = y - canvasConfig.y;
+    return [newX, newY];
 }
 
 function copyCanvas(srcCanvas, srcOrder, destCanvas, destOrder,
@@ -167,16 +221,16 @@ function drawLine(context, [x1, y1], [x2, y2], lineStyle) {
 }
 
 function init() {
-    const cs = byId('cs');
-    console.log('cs', cs?.shadowRoot.firstElementChild.firstElementChild);
-    const dims = { size: { w: 200, h: 400 }, margin: { left: 20, top: 0 } };
+    const dims = { size: { w: 400, h: 400 },
+                   margin: { left: 20, top: 10 } };
     const canvasParent = byId('canvas-parent');
     const canvasCount = 3;
-    const globalState = { page: p1, lineStyle: {strokeStyle: 'blue',
+    const globalState = { page: p1, lineStyle: {strokeStyle: 'black',
                                                 lineWidth: 5} };
     const sheet = createSheet(canvasParent, canvasCount,
                               dims, globalState);
-    addDrawingListeners(sheet, dims)
+    addDrawingListeners(sheet, dims);
+    addButtons(canvasParent, sheet.canvases, globalState);
 }
 window.addEventListener('load', init);
 
